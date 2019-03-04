b194274f8b19cf57b7d44e2bec009911cce7b64f
Robert Douglas <rob@anucreative.com>
2019-03-04 08:54:35 +0100

    Run 3 times and return median (middle) value
    Signed-off-by: Robert Douglas <robert@wttj.co>

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ const config = {
 }
 
 // Run Chrome and Lighthouse
-const launchChromeAndRunLighthouse = (url, opts, config = null) => {
+const launchChromeAndRunLighthouse = (url, opts, config = null, count) => {
+  console.log(`${count}. Running Lighthouse report for ${url}`)
+
   return chromeLauncher.launch({ chromeFlags: opts.chromeFlags }).then(chrome => {
     opts.port = chrome.port
     return lighthouse(url, opts, config)
@@ -39,16 +41,29 @@ const launchChromeAndRunLighthouse = (url, opts, config = null) => {
   })
 }
 
-const timestamp = formatDate(Date.now(), 'YYYY-MM-DD_HH-mm-ss_SSS')
+let timestamp
+
+// Set timestamp
+const setTimestamp = results => {
+  // Set new timestamp
+  timestamp = formatDate(Date.now(), 'YYYY-MM-DD_HH-mm-ss_SSS')
+  return Promise.resolve()
+}
 
 // Write original file
 const writeFile = results => {
+  console.log(
+    `   - Writing original report to reports/lighthouse/lighthouse.${timestamp}.result.json`
+  )
+
   fs.writeFileSync(`reports/lighthouse/lighthouse.${timestamp}.result.json`, results.report)
   return results.lhr
 }
 
 // Write parsed file
 const writeParsedFile = parsed => {
+  console.log(`   - Writing parsed report to reports/lighthouse/lighthouse.${timestamp}.parsed.json`)
+
   fs.writeFileSync(`reports/lighthouse/lighthouse.${timestamp}.parsed.json`, parsed)
   return JSON.parse(parsed)
 }
@@ -64,6 +79,8 @@ const parseJSON = results => {
 
 // Pull out just the metrics we want
 const filterMetrics = metrics => {
+  console.log('   - Filtering metrics')
+
   return metrics.reduce((acc, item) => {
     if (ATTRIBUTES_TO_POST.includes(item.id)) {
       acc.push(item)
@@ -72,20 +89,43 @@ const filterMetrics = metrics => {
   }, [])
 }
 
+// Get `speed-index` value from run
+const getSpeedIndex = run => run.find(item => item.id === 'speed-index').rawValue
+
+// Get median (middle) value of runs
+const getMedian = metrics => {
+  console.log('*. Getting median value')
+  // Sort by `speed-index`
+  const sorted = metrics.sort((a, b) => getSpeedIndex(a) - getSpeedIndex(b))
+
+  // Calculate middle position (for 0-based array)
+  const middle = Math.ceil(metrics.length / 2) - 1
+
+  return sorted[middle]
+}
+
+// Single run of Lighthouse
+const singleRun = (url, count) =>
+  setTimestamp()
+    .then(() => launchChromeAndRunLighthouse(url, opts, config, count))
+    .then(writeFile)
+    .then(parseJSON)
+    .then(writeParsedFile)
+    .then(filterMetrics)
+    .catch(console.error)
+
 // Run everything
 const run = url => {
   if (!url) {
     throw Error('No URL provided')
   }
 
-  console.log('Running Lighthouse report for', url)
-
-  return launchChromeAndRunLighthouse(url, opts, config)
-    .then(writeFile)
-    .then(parseJSON)
-    .then(writeParsedFile)
-    .then(filterMetrics)
-    .catch(console.error)
+  return [1, 2, 3]
+    .reduce(
+      (chain, i) => chain.then(results => singleRun(url, i).then(result => [...results, result])),
+      Promise.resolve([])
+    )
+    .then(getMedian)
 }
 
 module.exports = {
